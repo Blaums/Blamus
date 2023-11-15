f2df13707198fc47838c34dd53d50428f6ce2126
Blaums <117294609+Blaums@users.noreply.github.com>
2023-11-15 23:56:20 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
-export const SITE_TITLE = 'Rihard'
+export const SITE_TITLE = 'Rihard Jānis Blaums'
 export const SITE_DESCRIPTION = 'Welcome to my homepage. Im a IT geek and I love to learn new stuff.'
 
-export const NAME = 'Rihard'
+export const NAME = 'Rihard Jānis Blaums'
 export const ROLE = 'IT support'
 export const BOOKS = ['Clean Code - Robert C. Martin', 'Metamorphosis - Franz Kafka', 'Fahrenheit 451 - Ray Bradbury', 'The Crow and Other Stories - Edgar Allan Poe']
 export const HOBBIES = ['Linux customization', 'gaming', 'reading', 'coding']
